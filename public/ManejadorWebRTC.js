5618c4c16661c54a5830da02719f8dac10cf4d15
Yesid <trigoperfec3@hotmail.com>
2020-07-29 13:08:34 -0500

    limpieza servidor de datos innecesarios

--- public/ManejadorWebRTC.js
+++
@@ -1,9 +0,0 @@
-/*obtener permisos del navegador web*/
-navigator.mediaDevices.getUserMedia({audio : true, video : true})
-    .then(stream =>{
-        window.localStream = stream;
-    })
-    .catch((error)=>{
-        console.log(error);
-    })
-    ;
