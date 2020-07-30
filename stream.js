5b7d88edf9f15bb0107abc33d4e34cc201296e9f
Yesid <trigoperfec3@hotmail.com>
2020-07-30 03:03:21 -0500

    implementacion de metodo que convierte data a objeto json

--- stream.js
+++ stream.js
@@ -12,42 +12,45 @@ function suscripcion(nombre,nuevoUsuario,socket){
     socket.on(nombre,(data)=>{
         //subscribirse unirse a sala
         
-        const jsonObject = JSON.parse(`${data}`)    
-        
-        socket.join(jsonObject.room);
-        socket.join(jsonObject.socketId);
+                
+        socket.join(convertirAJson(data).room);
+        socket.join(convertirAJson(data).socketId);
 
-        if( socket.adapter.rooms[jsonObject.room].length > 1  ){
-            console.log(`room : ${jsonObject.room}`);
-            socket.to(jsonObject.room).emit(nuevoUsuario,{socketId : jsonObject.socketId });
+        if( socket.adapter.rooms[convertirAJson(data).room].length > 1  ){
+            console.log(`room : ${convertirAJson(data).room}`);
+            socket.to(convertirAJson(data).room).emit(nuevoUsuario,{socketId : convertirAJson(data).socketId });
         }
 
     });
 }
 
+function convertirAJson(data){
+    return JSON.parse(`${data}`)
+}
+
 function newUserStart(nombre,socket){
     socket.on(nombre,(data)=>{
-        socket.to(data.to).emit(nombre,{ sender : data.sender });
+        socket.to(convertirAJson(data).to).emit(nombre,{ sender : convertirAJson(data).sender });
     });
 }
 
 function sdp(nombre,socket){
     socket.on(nombre,(data)=>{
-        socket.to(data.to).emit(nombre, { description : data.description, sender : data.sender });
+        socket.to(convertirAJson(data).to).emit(nombre, { description : convertirAJson(data).description, sender : convertirAJson(data).sender });
     });
 }
 
 function iceCandidates(nombre,socket){
 
     socket.on(nombre,(data)=>{
-        socket.to(data.to).emit(nombre,{ candidate : data.candidate, sender : data.sender });
+        socket.to(convertirAJson(data).to).emit(nombre,{ candidate : convertirAJson(data).candidate, sender : convertirAJson(data).sender });
     })
 
 }
 
 function chat(nombre,socket){
     socket.on(nombre,(data)=>{
-        socket.to(data.room).emit(nombre,{ sender : data.sender, msg : data.msg });
+        socket.to(convertirAJson(data).room).emit(nombre,{ sender : convertirAJson(data).sender, msg : convertirAJson(data).msg });
     });
 }
 
